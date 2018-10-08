484264e8460288b03d445651f56b770b0262c37a
26thSpring <foelsk56@gmail.com>
2018-10-08 16:47:47 +0900

    모바일 이미지 개선작업

--- src/components/HomeHeader/HomeHeader.js
+++ src/components/HomeHeader/HomeHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomeHeader.scss';
+import thumbnailImage from 'static/thumbnail.jpg';
 
 const HomeHeader = () => {
    return (
@@ -7,10 +8,7 @@ const HomeHeader = () => {
          <div className="HomeHeader_background" />
          <span>프론트엔드의 미래는 밝다 :)</span>
          <div className="HomeHeader_profile">
-            <img
-               src="http://shinmorae.com/files/gimgs/12_nov_2016.jpg"
-               alt=""
-            />
+            <img src={thumbnailImage} alt="thumbnail" />
             @박찬욱
          </div>
       </div>
